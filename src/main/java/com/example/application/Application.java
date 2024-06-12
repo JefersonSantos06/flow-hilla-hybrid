6d708b060ee4ddb47c919b7a58a5505bc85bb4d6
Jeferson Santos <jeferson@t3w.io>
2024-06-12 14:00:05 -0300

    Adicionado log para exibir a URL do servidor local quando nenhum profile está ativo.

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -3,26 +3,24 @@ package com.example.application;
 import com.example.application.data.UserRepository;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
-import javax.sql.DataSource;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
 import org.springframework.context.annotation.Bean;
 
-/**
- * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
- */
+import javax.sql.DataSource;
+
 @SpringBootApplication
 @Theme(value = "my-app")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        final var context = SpringApplication.run(Application.class, args);
+        if (context.getEnvironment().getActiveProfiles().length == 0) {
+            Loggable.getLogger(Application.class).debug("http://localhost:{}", ObjectUtils.firstNonNull(context.getEnvironment().getProperty("local.server.port"), "8080"));
+        }
     }
 
     @Bean
